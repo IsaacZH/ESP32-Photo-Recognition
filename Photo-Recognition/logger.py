64c72572be9da92079bd37984f1cc99bd73df397
Isaac <1812924685@qq.com>
2025-02-11 21:25:27 +0800

    [Refactor] Update logger messages to English and streamline logging functionality

--- Photo-Recognition/logger.py
+++ Photo-Recognition/logger.py
@@ -9,17 +9,17 @@ class Logger:
         self.min_level = min_level
     
     def log(self, message, level="INFO"):
-        # 自定义日志级别
+        # Custom log levels
         levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
         
         if levels.index(level) >= levels.index(self.min_level):
-            runtime = time.ticks_ms()  # 获取系统运行时间（毫秒）
+            runtime = time.ticks_ms()  # Get system runtime (milliseconds)
             log_message = "{} - {} - {}".format(runtime, level, message)
             
-            # 输出到控制台
+            # Output to console
             print(log_message)
             
-            # 如果设置了日志文件名，写入文件
+            # If a log filename is set, write to file
             if self.filename:
                 with open(self.filename, 'a') as log_file:
                     log_file.write(log_message + "\n")
@@ -39,6 +39,6 @@ class Logger:
     def critical(self, *args):
         self.log(self._format_message(*args), level="CRITICAL")
     
-    # 新增方法来处理多个参数，将它们连接成一个字符串
+    # New method to handle multiple arguments and concatenate them into a string
     def _format_message(self, *args):
         return ' '.join(str(arg) for arg in args)
